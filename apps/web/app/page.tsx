b66072048e3adb023003752c397a6d20a400399b
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-17 19:07:29 +0200

    conncet convex to widget

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,8 +6,11 @@ import { useQuery } from "convex/react";
 export default function Page() {
   const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p className="">apps/web</p>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </div>
     </div>
   );
 }
