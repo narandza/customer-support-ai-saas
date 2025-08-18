413897bc683fe74dbea60cc3c9aac26b19d05abc
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-18 18:25:42 +0200

    add to sign-in view

--- apps/web/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,8 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 import { SignIn } from "@clerk/nextjs";
 
 const Page = () => {
-  return <SignIn />;
+  return <SignInView />;
 };
 
 export default Page;
