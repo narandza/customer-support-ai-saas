9dec051b6287b98756eef4c40bfe579d3e2bb2f4
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-18 18:41:08 +0200

    fix catch all error

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import {
-  Authenticated,
-  Unauthenticated,
-  useMutation,
-  useQuery,
-} from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { SignIn, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
@@ -16,22 +11,16 @@ export default function Page() {
 
   return (
     <>
-      <Authenticated>
-        <div className="flex flex-col items-center justify-center min-h-svh">
-          <p className="">apps/web</p>
-          <Button onClick={() => addUser()}>Add user</Button>
-          <div className="max-w-sm w-full mx-auto">
-            {JSON.stringify(users, null, 2)}
-          </div>
-          <div className="">
-            <UserButton />
-          </div>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p className="">apps/web</p>
+        <Button onClick={() => addUser()}>Add user</Button>
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
         </div>
-      </Authenticated>
-      <Unauthenticated>
-        Must be signed in
-        <SignIn />
-      </Unauthenticated>
+        <div className="">
+          <UserButton />
+        </div>
+      </div>
     </>
   );
 }
