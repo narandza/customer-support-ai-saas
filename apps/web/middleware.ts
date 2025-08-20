698d54ab16b4509b7e6adf6aa7909a6aecf8fa60
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-20 19:46:12 +0200

    fix merge conflict

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -6,25 +6,14 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 const isOrgFreeRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/org-selection(.*)",
+  "/org-selection(*.)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, orgId } = await auth();
+  const { userId, orgId } = await auth;
 
   if (!isPublicRoute(req)) {
     await auth.protect();
-  }
-
-  if (userId && !orgId && !isOrgFreeRoute(req)) {
-    const searchParams = new URLSearchParams({ redirectURL: req.url });
-
-    const orgSelection = new URL(
-      `/org-selection?${searchParams.toString()}`,
-      req.url
-    );
-
-    return NextResponse.redirect(orgSelection);
   }
 });
 
