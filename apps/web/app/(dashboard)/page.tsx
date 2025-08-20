a3e2255b2d6b99af5013890792819638bee7ab9e
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-20 19:48:04 +0200

    revert sentry implementationi

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 import { OrganizationSwitcher, SignIn, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
   return (
@@ -14,6 +15,9 @@ export default function Page() {
         <p className="">apps/web</p>
         <OrganizationSwitcher hidePersonal />
         <Button onClick={() => addUser()}>Add user</Button>
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
+        </div>
         <div className="">
           <UserButton />
         </div>
