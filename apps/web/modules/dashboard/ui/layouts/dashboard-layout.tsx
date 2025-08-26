ee77ed29bc13811f935a806083ee756b800dfd79
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-26 12:35:16 +0200

    add sidebar provider

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -1,15 +1,22 @@
 import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
 import { OrgGuard } from "@/modules/auth/ui/components/org-guard";
+import { SidebarProvider } from "@workspace/ui/components/sidebar";
+import { cookies } from "next/headers";
 
-export const DashboardLayout = ({
+export const DashboardLayout = async ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
   return (
     <AuthGuard>
       <OrgGuard>
-        <main className="flex flex-1 flex-col">{children}</main>
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <main className="flex flex-1 flex-col">{children}</main>
+        </SidebarProvider>
       </OrgGuard>
     </AuthGuard>
   );
