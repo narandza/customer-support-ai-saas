88ec49ab42565dae86ff8abfef4a1b4475b32507
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-26 12:21:58 +0200

    implement layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,12 +1,7 @@
-import { OrgGuard } from "@/modules/auth/ui/components/org-guard";
-import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboard-layout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <AuthGuard>
-      <OrgGuard>{children}</OrgGuard>
-    </AuthGuard>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 };
 
 export default Layout;
