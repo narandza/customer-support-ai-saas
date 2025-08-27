018fcdc0368c74f48b4eeab3e67975205baea67f
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-27 12:26:00 +0200

    add customer support items

---
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { InboxIcon, LibraryBigIcon } from "lucide-react";
+
+const customerSupportItems = [
+  {
+    title: "Conversations",
+    url: "/conversations",
+    icon: InboxIcon,
+  },
+  {
+    title: "Knowledge base",
+    url: "/files",
+    icon: LibraryBigIcon,
+  },
+];
