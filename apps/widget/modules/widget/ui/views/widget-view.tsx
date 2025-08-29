478e9251dc1b044b35f5efc9449e0cc40c83fee2
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-29 15:24:00 +0200

    add intro messsage

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -10,7 +10,12 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className="flex min-h-screen min-w-screen h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetHeader>header</WidgetHeader>
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+          <p className="text-3xl">Hi there! 👋</p>
+          <p className=" text-lg">How can we help you today?</p>
+        </div>
+      </WidgetHeader>
       <div className="flex flex-1">Widget View {organizationId}</div>
       <WidgetFooter />
     </main>
