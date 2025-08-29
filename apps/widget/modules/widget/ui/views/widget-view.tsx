321a396b458dbf288a4459fbd2e7ff5e86438514
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-29 15:21:13 +0200

    upadte widget view

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { WidgetFooter } from "../components/widget-footer";
+import { WidgetHeader } from "../components/widget-header";
+
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
   return (
-    <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      Widget View {organizationId}
+    <main className="flex min-h-screen min-w-screen h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+      <WidgetHeader>header</WidgetHeader>
+      <div className="flex flex-1">Widget View {organizationId}</div>
+      <WidgetFooter />
     </main>
   );
 };
