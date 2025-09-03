7c8711536933d254987dfa935bb7c4754c36c9a2
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-03 12:50:19 +0200

    build widget error screen

--- apps/widget/modules/widget/atoms/widget-atoms.ts
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -4,3 +4,5 @@ import { WidgetScreen } from "../types";
 
 // Basic widget state atoms
 export const screenAtom = atom<WidgetScreen>("loading");
+
+export const errorMessageAtom = atom<string | null>(null);
