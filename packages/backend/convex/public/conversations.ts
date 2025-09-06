fac147c51e3f5badaf8ac25d5b148a2c4c76cbb1
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-06 23:15:01 +0200

    build conversations api call

---
+++ packages/backend/convex/public/conversations.ts
@@ -0,0 +1,31 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "../_generated/server";
+
+export const create = mutation({
+  args: {
+    organizationId: v.string(),
+    contactSessionId: v.id("contactSessions"),
+  },
+  handler: async (ctx, args) => {
+    const session = await ctx.db.get(args.contactSessionId);
+
+    if (!session || session.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid session",
+      });
+    }
+
+    // TODO: Replace once functionality for thread creation is present
+    const threadId = "123";
+
+    const conversationId = await ctx.db.insert("conversations", {
+      contactSessionId: session._id,
+      status: "unresolved",
+      organizationId: args.organizationId,
+      threadId,
+    });
+
+    return conversationId;
+  },
+});
