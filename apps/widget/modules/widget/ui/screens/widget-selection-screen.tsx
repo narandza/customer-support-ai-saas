695d8da14dceab74c64efea1ded0e7e830d342da
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-07 13:10:39 +0200

    Add button pending control

--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -12,6 +12,7 @@ import {
 } from "../../atoms/widget-atoms";
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { useState } from "react";
 
 export const WidgetSelectionScreen = () => {
   const setScreen = useSetAtom(screenAtom);
@@ -22,6 +23,7 @@ export const WidgetSelectionScreen = () => {
   );
 
   const createConversation = useMutation(api.public.conversations.create);
+  const [isPending, setIsPending] = useState(false);
 
   const handleNewConversation = async () => {
     if (!organizationId) {
@@ -35,6 +37,7 @@ export const WidgetSelectionScreen = () => {
       return;
     }
 
+    setIsPending(true);
     try {
       const conversationId = await createConversation({
         contactSessionId,
@@ -44,6 +47,8 @@ export const WidgetSelectionScreen = () => {
       setScreen("chat");
     } catch {
       setScreen("auth");
+    } finally {
+      setIsPending(false);
     }
   };
 
@@ -60,6 +65,7 @@ export const WidgetSelectionScreen = () => {
           className="h-16 w-full justify-between"
           variant="outline"
           onClick={handleNewConversation}
+          disabled={isPending}
         >
           <div className="flex items-center gap-x-2">
             <MessageSquareTextIcon className="size-4" />
