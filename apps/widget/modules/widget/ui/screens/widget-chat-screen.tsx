852cadd92a31fe94bf3a6bc158a50a3fc419b62b
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-08 21:35:05 +0200

    fetch conversations

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -5,8 +5,30 @@ import { useAtomValue } from "jotai";
 import { WidgetHeader } from "../components/widget-header";
 import { Button } from "@workspace/ui/components/button";
 import { ArrowLeftIcon, MenuIcon } from "lucide-react";
+import {
+  contactSessionIdAtomFamily,
+  conversationIdAtom,
+  organizationIdAtom,
+} from "../../atoms/widget-atoms";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export const WidgetChatScreen = () => {
+  const conversationId = useAtomValue(conversationIdAtom);
+  const organizationId = useAtomValue(organizationIdAtom);
+  const contactSessionId = useAtomValue(
+    contactSessionIdAtomFamily(organizationId || "")
+  );
+
+  const conversation = useQuery(
+    api.public.conversations.getOne,
+    conversationId && contactSessionId
+      ? {
+          conversationId,
+          contactSessionId,
+        }
+      : "skip"
+  );
   return (
     <>
       <WidgetHeader className="flex items-center justify-between">
