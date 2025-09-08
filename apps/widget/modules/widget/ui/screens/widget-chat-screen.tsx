13fbb4f552619b7f8eef8aa204fd5e859631fe90
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-08 21:30:35 +0200

    build widget chat screen header

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -4,17 +4,21 @@ import { useAtomValue } from "jotai";
 
 import { WidgetHeader } from "../components/widget-header";
 import { Button } from "@workspace/ui/components/button";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, MenuIcon } from "lucide-react";
 
 export const WidgetChatScreen = () => {
   return (
     <>
       <WidgetHeader className="flex items-center justify-between">
         <div className="flex items-center gap-x-2">
-          <Button size="icon">
+          <Button size="icon" variant="transparent">
             <ArrowLeftIcon />
           </Button>
+          <p className="">Chat</p>
         </div>
+        <Button size="icon" variant="transparent">
+          <MenuIcon />
+        </Button>
       </WidgetHeader>
       <div className="flex flex-1 flex-col gap-y-4 p-4 ">Chat Screen</div>
     </>
