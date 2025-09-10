49acff1e0dc622695cb4023fae4b280d774d83e7
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-10 20:39:25 +0200

    fix conversations get one csecurity

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -20,7 +20,17 @@ export const getOne = query({
     const conversation = await ctx.db.get(args.conversationId);
 
     if (!conversation) {
-      return null;
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+
+    if (conversation.contactSessionId !== session._id) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Incorrect session",
+      });
     }
 
     return {
