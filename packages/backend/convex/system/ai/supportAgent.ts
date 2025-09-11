2cd9803306634a7b0f365309dd7c421f6b0e85f7
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-11 16:39:48 +0200

    build support agent call

---
+++ packages/backend/convex/system/ai/supportAgent.ts
@@ -0,0 +1,8 @@
+import { components } from "../../_generated/api";
+import { Agent } from "@convex-dev/agent";
+import { openai } from "@ai-sdk/openai";
+
+const agent = new Agent(components.agent, {
+  name: "My Agent",
+  languageModel: openai.chat("gpt-4o-mini"),
+});
