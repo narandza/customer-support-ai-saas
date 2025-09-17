ec6b1b82462b9ceeeaacf86859a99143bea84694
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-17 22:34:57 +0200

    Fix depoendecy error, praise the lord

--- packages/backend/convex/system/ai/supportAgent.ts
+++ packages/backend/convex/system/ai/supportAgent.ts
@@ -1,3 +1,8 @@
 import { components } from "../../_generated/api";
 import { Agent } from "@convex-dev/agent";
 import { openai } from "@ai-sdk/openai";
+
+const agent = new Agent(components.agent, {
+  chat: openai.chat("gpt-4o-mini"),
+  instructions: "You are a customer support agent",
+});
