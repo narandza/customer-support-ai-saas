29c31ac266dbf3c1d8a26239b8493f29c95f3bac
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-19 15:33:34 +0200

    build getone message action

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -10,8 +10,11 @@
 
 import type * as public_contactSessions from "../public/contactSessions.js";
 import type * as public_conversations from "../public/conversations.js";
+import type * as public_messages from "../public/messages.js";
 import type * as public_organizations from "../public/organizations.js";
 import type * as system_ai_agents_supportAgent from "../system/ai/agents/supportAgent.js";
+import type * as system_contactSessions from "../system/contactSessions.js";
+import type * as system_conversations from "../system/conversations.js";
 import type * as users from "../users.js";
 
 import type {
@@ -31,8 +34,11 @@ import type {
 declare const fullApi: ApiFromModules<{
   "public/contactSessions": typeof public_contactSessions;
   "public/conversations": typeof public_conversations;
+  "public/messages": typeof public_messages;
   "public/organizations": typeof public_organizations;
   "system/ai/agents/supportAgent": typeof system_ai_agents_supportAgent;
+  "system/contactSessions": typeof system_contactSessions;
+  "system/conversations": typeof system_conversations;
   users: typeof users;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
