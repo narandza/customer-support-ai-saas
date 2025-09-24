df5f7cb7ebdfa48e391f7fc27bb7c8fa13f174b4
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-24 17:15:34 +0200

    build widget inbox screen layout

---
+++ apps/widget/modules/widget/ui/screens/widget-inbox-screen.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useAtomValue } from "jotai";
+import { AlertTriangleIcon } from "lucide-react";
+
+import { WidgetHeader } from "../components/widget-header";
+import { errorMessageAtom } from "../../atoms/widget-atoms";
+
+export const WidgetInboxScreen = () => {
+  return (
+    <>
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+          <p className="text-3xl">Hi there! 👋</p>
+          <p className=" text-lg">Let&apos;s get you started</p>
+        </div>
+      </WidgetHeader>
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-4 p-4 text-muted-foreground">
+        Inbox
+      </div>
+    </>
+  );
+};
