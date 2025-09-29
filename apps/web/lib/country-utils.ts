9ecd33e658078ce62c51a389e465569874056d1e
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-09-29 20:33:24 +0200

    build get coounty flag url util

--- apps/web/lib/country-utils.ts
+++ apps/web/lib/country-utils.ts
@@ -20,3 +20,7 @@ export function getCountryFromTimezone(timezone?: string) {
     name: country?.name || countryCode,
   };
 }
+
+export function getCountryFlagUrl(countryCode: string) {
+  return `https://flagcdn.com/w40/${countryCode.toLocaleLowerCase()}.png`;
+}
