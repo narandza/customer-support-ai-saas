e6ea140290fd858b5db1d79496edc6a372c31c1c
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-01 20:03:25 +0200

    define form

--- apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
@@ -5,10 +5,21 @@ import { Id } from "@workspace/backend/_generated/dataModel";
 import { Button } from "@workspace/ui/components/button";
 import { useQuery } from "convex/react";
 import { MoreHorizontalIcon } from "lucide-react";
+import { AIConversation } from "@workspace/ui/components/ai/conversation";
+import { AIInput } from "@workspace/ui/components/ai/input";
+import { AIMessage } from "@workspace/ui/components/ai/message";
+import { AIResponse } from "@workspace/ui/components/ai/response";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface ConversationIdViewProps {
   conversationId: Id<"conversations">;
 }
+
+const formSchema = z.object({
+  message: z.string().min(1, "Message is required"),
+});
 
 export const ConversationIdView = ({
   conversationId,
@@ -16,6 +27,17 @@ export const ConversationIdView = ({
   const conversation = useQuery(api.private.conversations.getOne, {
     conversationId,
   });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="flex h-full flex-col bg-muted">
       <div className="flex items-center justify-between border-b bg-background p-2.5">
