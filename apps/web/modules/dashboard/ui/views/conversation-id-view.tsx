8c3bb3864659c41134c5041b3ee67372dcb2963d
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-09 09:04:27 +0200

    build messsages form

--- apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
@@ -4,13 +4,20 @@ import { api } from "@workspace/backend/_generated/api";
 import { Id } from "@workspace/backend/_generated/dataModel";
 import { Button } from "@workspace/ui/components/button";
 import { useQuery } from "convex/react";
-import { MoreHorizontalIcon } from "lucide-react";
+import { MoreHorizontalIcon, Wand2Icon } from "lucide-react";
 import {
   AIConversation,
   AIConversationContent,
   AIConversationScrollButton,
 } from "@workspace/ui/components/ai/conversation";
-import { AIInput } from "@workspace/ui/components/ai/input";
+import {
+  AIInput,
+  AIInputButton,
+  AIInputSubmit,
+  AIInputTextarea,
+  AIInputToolbar,
+  AIInputTools,
+} from "@workspace/ui/components/ai/input";
 import {
   AIMessage,
   AIMessageContent,
@@ -21,6 +28,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toUIMessages, useThreadMessages } from "@convex-dev/agent/react";
 import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar";
+import { Form, FormField } from "@workspace/ui/components/form";
 
 interface ConversationIdViewProps {
   conversationId: Id<"conversations">;
@@ -82,6 +90,55 @@ export const ConversationIdView = ({
         </AIConversationContent>
         <AIConversationScrollButton />
       </AIConversation>
+
+      <div className="p-2">
+        <Form {...form}>
+          <AIInput onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              disabled={conversation?.status === "resolved"}
+              name="message"
+              render={({ field }) => (
+                <AIInputTextarea
+                  disabled={
+                    conversation?.status === "resolved" ||
+                    form.formState.isSubmitting
+                  } // TODO: OR if enhancing prompt
+                  onChange={field.onChange}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && !e.shiftKey) {
+                      e.preventDefault();
+                      form.handleSubmit(onSubmit)();
+                    }
+                  }}
+                  placeholder={
+                    conversation?.status === "resolved"
+                      ? "This conversation has been resolved"
+                      : "Type your response as on operator..."
+                  }
+                  value={field.value}
+                />
+              )}
+            />
+            <AIInputToolbar>
+              <AIInputTools>
+                <AIInputButton>
+                  <Wand2Icon /> Enhance
+                </AIInputButton>
+              </AIInputTools>
+              <AIInputSubmit
+                disabled={
+                  conversation?.status === "resolved" ||
+                  !form.formState.isValid ||
+                  form.formState.isSubmitting
+                } // TODO: OR if enhancing prompt
+                status="ready"
+                type="submit"
+              />
+            </AIInputToolbar>
+          </AIInput>
+        </Form>
+      </div>
     </div>
   );
 };
