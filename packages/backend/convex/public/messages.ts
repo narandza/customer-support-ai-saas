d6f79f40321e5de545b7e9778bb4c8dc3ee00059
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-14 07:31:50 +0200

    add agent trigger check

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -1,11 +1,12 @@
 import { ConvexError, v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
 
-import { internal } from "../_generated/api";
+import { components, internal } from "../_generated/api";
 import { action, query } from "../_generated/server";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { escalateConversation } from "../system/ai/tools/escalateConversation";
 import { resolveConversation } from "../system/ai/tools/resolveConversation";
+import { saveMessage } from "@convex-dev/agent";
 
 export const create = action({
   args: {
@@ -50,20 +51,28 @@ export const create = action({
     }
 
     // TODO: Implement subscription check
+    const shouldTriggerAgent = conversation.status === "unresolved";
 
-    await supportAgent.generateText(
-      ctx,
-      {
-        threadId: args.threadId,
-      },
-      {
-        prompt: args.prompt,
-        tools: {
-          escalateConversation,
-          resolveConversation,
+    if (shouldTriggerAgent) {
+      await supportAgent.generateText(
+        ctx,
+        {
+          threadId: args.threadId,
         },
-      }
-    );
+        {
+          prompt: args.prompt,
+          tools: {
+            escalateConversation,
+            resolveConversation,
+          },
+        }
+      );
+    } else {
+      await saveMessage(ctx, components.agent, {
+        threadId: args.threadId,
+        prompt: args.prompt,
+      });
+    }
   },
 });
 
