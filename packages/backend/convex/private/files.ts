37c6d366c2db254fd23d8a3633e75ff32742ca2b
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-11-03 17:33:11 +0100

    build guess mime type util

--- packages/backend/convex/private/files.ts
+++ packages/backend/convex/private/files.ts
@@ -1,5 +1,17 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
+import {
+  guessMimeTypeFromContents,
+  guessMimeTypeFromExtension,
+} from "@convex-dev/rag";
 import { action } from "../_generated/server";
+
+function guessMimeType(filename: string, bytes: ArrayBuffer): string {
+  return (
+    guessMimeTypeFromExtension(filename) ||
+    guessMimeTypeFromContents(bytes) ||
+    "application/octet-stream"
+  );
+}
 
 export const addFile = action({
   args: {
@@ -26,5 +38,9 @@ export const addFile = action({
         message: "Organization not found",
       });
     }
+
+    const { bytes, filename, category } = args;
+
+    const mimeType = args.mimeType || guessMimeType(filename, bytes);
   },
 });
