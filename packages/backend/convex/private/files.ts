21f999798b4c8fc2b6019426888c4df7afabbf9e
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-11-03 17:31:27 +0100

    add rag convex package

---
+++ packages/backend/convex/private/files.ts
@@ -0,0 +1,30 @@
+import { v } from "convex/values";
+import { action } from "../_generated/server";
+
+export const addFile = action({
+  args: {
+    filename: v.string(),
+    mimeType: v.string(),
+    bytes: v.bytes(),
+    category: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+  },
+});
