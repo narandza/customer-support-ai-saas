dfc4fb594e01b50b518bcd6c84ac2e7bc9e66c76
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-11-13 19:04:10 +0100

    build delete file funcition

--- packages/backend/convex/private/files.ts
+++ packages/backend/convex/private/files.ts
@@ -3,10 +3,12 @@ import {
   contentHashFromArrayBuffer,
   guessMimeTypeFromContents,
   guessMimeTypeFromExtension,
+  vEntryId,
 } from "@convex-dev/rag";
-import { action } from "../_generated/server";
+import { action, mutation } from "../_generated/server";
 import { extractTextContent } from "../lib/extractTextContent";
 import rag from "../system/ai/rag";
+import { Id } from "../_generated/dataModel";
 
 function guessMimeType(filename: string, bytes: ArrayBuffer): string {
   return (
@@ -15,6 +17,66 @@ function guessMimeType(filename: string, bytes: ArrayBuffer): string {
     "application/octet-stream"
   );
 }
+
+export const deleteFile = mutation({
+  args: { entryId: vEntryId },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    const namespace = await rag.getNamespace(ctx, {
+      namespace: orgId,
+    });
+
+    if (!namespace) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid namespace",
+      });
+    }
+
+    const entry = await rag.getEntry(ctx, {
+      entryId: args.entryId,
+    });
+
+    if (!entry) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Entry not found",
+      });
+    }
+
+    if (entry.metadata?.uploadedBy !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid organization ID",
+      });
+    }
+
+    if (entry.metadata.storageId) {
+      await ctx.storage.delete(entry.metadata.storageId as Id<"_storage">);
+    }
+
+    await rag.deleteAsync(ctx, {
+      entryId: args.entryId,
+    });
+  },
+});
 
 export const addFile = action({
   args: {
@@ -64,8 +126,8 @@ export const addFile = action({
       text,
       key: filename,
       metadata: {
-        storageId,
-        uploadedBy: orgId,
+        storageId, // Important for file deletion
+        uploadedBy: orgId, // Important for deletion
         filename,
         category: category ?? null,
       },
