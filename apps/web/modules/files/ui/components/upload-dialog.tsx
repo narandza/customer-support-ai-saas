af31c7810e52b92d942ce190033ad185b73c2e4a
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-11-18 04:48:09 +0100

    initiate upload dialog compoennt

--- apps/web/modules/files/ui/components/upload-dialog.tsx
+++ apps/web/modules/files/ui/components/upload-dialog.tsx
@@ -1,3 +1,48 @@
 "use client";
 
 import { Dialog } from "@workspace/ui/components/dialog";
+import { useAction } from "convex/react";
+import { Input } from "@workspace/ui/components/input";
+import { Label } from "@workspace/ui/components/label";
+import { Button } from "@workspace/ui/components/button";
+import { Dropzone } from "@workspace/ui/components/dropzone";
+import { api } from "@workspace/backend/_generated/api";
+import { useState } from "react";
+
+interface UploadDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onFileUploaded?: () => void;
+}
+
+export const UploadDialog = ({
+  open,
+  onOpenChange,
+  onFileUploaded,
+}: UploadDialogProps) => {
+  const addFile = useAction(api.private.files.addFile);
+
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadForm, setUploadForm] = useState({
+    category: "",
+    filename: "",
+  });
+
+  const handleFileDrop = (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+
+    if (file) {
+      setUploadedFiles([file]);
+
+      if (!uploadForm.filename) {
+        setUploadForm((prev) => ({
+          ...prev,
+          filename: file.name,
+        }));
+      }
+    }
+  };
+
+  return <Dialog></Dialog>;
+};
