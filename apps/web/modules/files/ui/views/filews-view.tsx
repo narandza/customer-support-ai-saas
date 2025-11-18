8b7246eb89cb95153e6aa2768d77f1a8e9eeeb36
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-11-18 04:15:05 +0100

    build files view header

--- apps/web/modules/files/ui/views/filews-view.tsx
+++ apps/web/modules/files/ui/views/filews-view.tsx
@@ -1,5 +1,31 @@
 "use client";
+import { Table } from "@workspace/ui/components/table";
+
+import { useInfiniteScroll } from "@workspace/ui/hooks/use-infinite-scroll";
+import { InfiniteScrollTrigger } from "@workspace/ui/components/infinite-scroll-trigger";
+import { usePaginatedQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+
+import type { PublicFile } from "@workspace/backend/private/files";
 
 export const FilesView = () => {
-  return <div className="">Files View!</div>;
+  const files = usePaginatedQuery(
+    api.private.files.list,
+    {},
+    { initialNumItems: 10 }
+  );
+
+  return (
+    <div className="flex min-h-screen flex-col bg-muted p-8">
+      <div className="mx-auto w-full max-w-screen-md">
+        <div className="space-y-2">
+          <h1 className="text-2xl md:text-4xl">Knowledge Base</h1>
+          <p className="text-muted-foreground">
+            Upload and manage documents for your AI assistant
+          </p>
+        </div>
+      </div>
+      {JSON.stringify(files)}
+    </div>
+  );
 };
