7f32e9a8de872440a190e3c046ebba2eb77def87
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-12-03 09:48:59 +0100

    build vapi view

--- apps/web/app/(dashboard)/plugins/vapi/page.tsx
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -1,5 +1,7 @@
+import { VapiView } from "@/modules/plugins/views/vapi-view";
+
 const Page = () => {
-  return <div className="">Vapi</div>;
+  return <VapiView />;
 };
 
 export default Page;
---
+++ apps/web/modules/plugins/views/vapi-view.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export const VapiView = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-muted p-8">
+      <div className="mx-auto w-full max-w-screen-md">
+        <div className="space-y-2">
+          <h1 className="text-2xl md:text-4xl ">Vapi Plugin</h1>
+          <p className="text-muted-foreground">
+            Connect Vapi to enable AI voice calls and phone support
+          </p>
+        </div>
+
+        <div className="mt-8">TODO: Plugin Card</div>
+      </div>
+    </div>
+  );
+};
