95c1987083956e2c14962a17bf37a515397bdbda
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-12-04 08:38:29 +0100

    build private get one plugin api call

---
+++ packages/backend/convex/private/plugins.ts
@@ -0,0 +1,33 @@
+import { ConvexError, v } from "convex/values";
+import { query } from "../_generated/server";
+
+export const getOne = query({
+  args: {
+    service: v.union(v.literal("vapi")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    return await ctx.db
+      .query("plugins")
+      .withIndex("by_organization_id_and_service", (q) =>
+        q.eq("organizationId", orgId).eq("service", args.service)
+      );
+  },
+});
