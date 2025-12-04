f9973afa5b459b3e7dba11016ed720e7d631f4cb
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-12-04 08:41:15 +0100

    build private remove plugin api call

--- packages/backend/convex/private/plugins.ts
+++ packages/backend/convex/private/plugins.ts
@@ -1,5 +1,46 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "../_generated/server";
+import { mutation, query } from "../_generated/server";
+
+export const remove = mutation({
+  args: {
+    service: v.union(v.literal("vapi")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    const existingPlugin = await ctx.db
+      .query("plugins")
+      .withIndex("by_organization_id_and_service", (q) =>
+        q.eq("organizationId", orgId).eq("service", args.service)
+      )
+      .unique();
+
+    if (!existingPlugin) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Plugin not found",
+      });
+    }
+
+    await ctx.db.delete(existingPlugin._id);
+  },
+});
 
 export const getOne = query({
   args: {
@@ -28,6 +69,7 @@ export const getOne = query({
       .query("plugins")
       .withIndex("by_organization_id_and_service", (q) =>
         q.eq("organizationId", orgId).eq("service", args.service)
-      );
+      )
+      .unique();
   },
 });
