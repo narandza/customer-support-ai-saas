14f976ad8049949227df8e782c26217263d1db1b
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-12-16 13:24:09 +0100

    implement vapi plugin

--- apps/web/modules/plugins/ui/views/vapi-view.tsx
+++ apps/web/modules/plugins/ui/views/vapi-view.tsx
@@ -140,7 +140,7 @@ const VapiPluginForm = ({
               )}
             />
             <DialogFooter>
-              <Button>
+              <Button disabled={form.formState.isSubmitting} type="submit">
                 {form.formState.isSubmitting ? "Connecting..." : "Connect"}
               </Button>
             </DialogFooter>
@@ -155,31 +155,43 @@ export const VapiView = () => {
   const vapiPlugin = useQuery(api.private.plugins.getOne, { service: "vapi" });
 
   const [connectOpen, setConnectOpen] = useState(false);
+  const [removeOpen, setRemoveOpen] = useState(false);
+
+  const handleSubmit = () => {
+    if (vapiPlugin) {
+      setRemoveOpen(true);
+    } else {
+      setConnectOpen(true);
+    }
+  };
 
   return (
-    <div className="flex min-h-screen flex-col bg-muted p-8">
-      <div className="mx-auto w-full max-w-screen-md">
-        <div className="space-y-2">
-          <h1 className="text-2xl md:text-4xl ">Vapi Plugin</h1>
-          <p className="text-muted-foreground">
-            Connect Vapi to enable AI voice calls and phone support
-          </p>
-        </div>
+    <>
+      <VapiPluginForm open={connectOpen} setOpen={setConnectOpen} />
+      <div className="flex min-h-screen flex-col bg-muted p-8">
+        <div className="mx-auto w-full max-w-screen-md">
+          <div className="space-y-2">
+            <h1 className="text-2xl md:text-4xl ">Vapi Plugin</h1>
+            <p className="text-muted-foreground">
+              Connect Vapi to enable AI voice calls and phone support
+            </p>
+          </div>
 
-        <div className="mt-8">
-          {vapiPlugin ? (
-            <p>Connected</p>
-          ) : (
-            <PluginCard
-              serviceName="Vapi"
-              serviceImage="/vapi.jpg"
-              features={vapiFeatures}
-              isDisabled={vapiPlugin === undefined}
-              onSubmit={() => {}}
-            />
-          )}
+          <div className="mt-8">
+            {vapiPlugin ? (
+              <p>Connected</p>
+            ) : (
+              <PluginCard
+                serviceName="Vapi"
+                serviceImage="/vapi.jpg"
+                features={vapiFeatures}
+                isDisabled={vapiPlugin === undefined}
+                onSubmit={handleSubmit}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
